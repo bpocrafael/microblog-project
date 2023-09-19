f9f9fcd78592b4a220c57b04a7f232d0323c7c87
adrean-hipe <adrean@bpoc.co.jp>
2023-09-19 17:49:11 +0800

    Modified login UI and removed the header of the default UI from app

--- laravel-app/resources/views/auth/login.blade.php
+++ laravel-app/resources/views/auth/login.blade.php
@@ -3,19 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+        <div class="col-md-4">
+            <h1 class="text-center p-5">Microblog</h1>
+            <div class="card m-3">
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="row justify-content-center my-3">
+                            <div class="col-md">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +21,9 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <div class="row justify-content-center mb-1 ms-3 mb-3">
+                            <div class="col-md">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,8 +33,18 @@
                             </div>
                         </div>
 
+                        
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md">
+                                @if (Route::has('password.request'))
+                                    <a href="{{ route('password.request') }}">
+                                        {{ __('Forgot Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,22 +55,39 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="row p-2 mb-0 mt-3">
+                            <div class="col-md text-center">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+
+            <div class="m-3 mt-5 text-center">
+                <div class="">
+                    <div class="col-md">
+                        <p class="text">
+                            No account yet? Create account.
+                        </p>
+                    </div>
+                    <div class="row mb-0">
+                        <div class="col-md text-center">
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">
+                                <button class="btn btn-light">
+                                    {{ __('Create') }}
+                                </button>
+                            </a>
+                        @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </div>
