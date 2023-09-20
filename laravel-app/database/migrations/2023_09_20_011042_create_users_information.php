acf96f962d15b983e1feb7dff71fd6c0be3f96d3
mikco-hipe <mikco@bpoc.co.jp>
2023-09-20 10:36:30 +0800

    added foreign key to user_information

--- laravel-app/database/migrations/2023_09_20_011042_create_users_information.php
+++ laravel-app/database/migrations/2023_09_20_011042_create_users_information.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users_information', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name')->nullable();
             $table->text('bio')->nullable();
             $table->enum('gender', ['male', 'female', 'other'])->nullable();
             $table->timestamps();
+
         });
     }
 
