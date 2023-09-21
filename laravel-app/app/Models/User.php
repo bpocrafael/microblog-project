c945cc448df06b4f2a0536e3201137bd27bc87da
mikco-hipe <mikco@bpoc.co.jp>
2023-09-21 15:32:09 +0800

    Working database retrieval and insertion in login and registration

--- laravel-app/app/Models/User.php
+++ laravel-app/app/Models/User.php
@@ -44,9 +44,4 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function users_information()
-    {
-        return $this->hasOne(UsersInformation::class);
-    }
 }
