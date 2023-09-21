3018f5d6021876553950c6833be8195aad378c27
adrean-hipe <144191038+adrean-hipe@users.noreply.github.com>
2023-09-21 12:56:19 +0800

    Added routing for root diretory (#9)

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -14,10 +14,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
