39426f023d9709f94ff1a4a77dddc9fba03fe407
mikco-hipe <mikco@bpoc.co.jp>
2023-09-26 16:04:25 +0800

    Removed unnecessary comments

--- laravel-app/app/Observers/isVerifiedObserver.php
+++ laravel-app/app/Observers/isVerifiedObserver.php
@@ -7,14 +7,6 @@ use App\Models\User;
 class isVerifiedObserver
 {
     /**
-     * Handle the User "created" event.
-     */
-    public function created(User $user): void
-    {
-        //
-    }
-
-    /**
      * Handle the User "updated" event.
      */
     public function updating(User $user): void
@@ -22,29 +14,5 @@ class isVerifiedObserver
         if ($user->isDirty('email_verified_at') && $user->email_verified_at !== null) {
             $user->is_verified = true;
         }
-    }
-
-    /**
-     * Handle the User "deleted" event.
-     */
-    public function deleted(User $user): void
-    {
-        //
-    }
-
-    /**
-     * Handle the User "restored" event.
-     */
-    public function restored(User $user): void
-    {
-        //
-    }
-
-    /**
-     * Handle the User "force deleted" event.
-     */
-    public function forceDeleted(User $user): void
-    {
-        //
     }
 }
