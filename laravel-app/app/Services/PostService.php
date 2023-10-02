80a75978281b8c049b51c6a7c393a94f92ae878e
adrean-hipe <adrean@bpoc.co.jp>
2023-10-02 12:34:39 +0800

    added @param to  post service createpost

--- laravel-app/app/Services/PostService.php
+++ laravel-app/app/Services/PostService.php
@@ -9,9 +9,12 @@ class PostService
 {
     public function createPost(User $user, array $validatedData) : UserPost
     {
-		/** @var \App\Models\User $user */
+		/* 
+        * @var \App\Models\User $user
+        * @param array<string, Item> $items
+        */
         $post = $user->posts()->create([
-            'content' => (string) $validatedData['content'],
+            'content' => $validatedData['content'],
         ]);
 
         return $post;
