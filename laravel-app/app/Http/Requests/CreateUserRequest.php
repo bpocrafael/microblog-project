5fca50cdba222e66790073453e11d828bcc5400f
mikco-hipe <144191092+mikco-hipe@users.noreply.github.com>
2023-10-02 11:46:52 +0800

    Created error message and unit test for RegisterController (#34)
    * Created error messgae for RegisterController and unit test
    
    * added value type in messages() function in CreateUserRequest
    
    * fixed single line per use
    
    * removed a single space that disrupts the cs fixer

--- laravel-app/app/Http/Requests/CreateUserRequest.php
+++ laravel-app/app/Http/Requests/CreateUserRequest.php
@@ -21,4 +21,26 @@ class CreateUserRequest extends FormRequest
             'last_name' => ['required', 'string', 'max:255'],
         ];
     }
+
+    /**
+     * Error messages for registration validation.
+     *
+     * @return array<string, mixed>
+     */
+    public function messages(): array
+    {
+        return [
+            'first_name.required' => 'The first name field is required.',
+            'last_name.required' => 'The last name field is required.',
+            'username.required' => 'The username field is required.',
+            'username.unique' => 'The username has already been taken.',
+            'email.required' => 'The email address field is required.',
+            'email.email' => 'The email address must be a valid email address.',
+            'email.unique' => 'The email address has already been taken.',
+            'password.required' => 'The password field is required.',
+            'password.min' => 'The password must be at least :min characters.',
+            'password.confirmed' => 'The password confirmation does not match.',
+        ];
+    }
+
 }
---
+++ laravel-app/tests/Feature/RegisterControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use App\Http\Requests\CreateUserRequest;
+
+class RegisterControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+    use InteractsWithDatabase;
+
+    /** @test */
+    public function test_valid_register_controller_test()
+    {
+        $data = [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'username' => $this->faker->userName,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'secret123',
+            'password_confirmation' => 'secret123',
+        ];
+
+        $request = new CreateUserRequest();
+        $validator = validator($data, $request->rules(), $request->messages());
+
+        $this->assertFalse($validator->fails());
+    }
+
+    /* @test */
+    public function test_invalid_register_controller_test()
+    {
+        $data = [
+            'first_name' => '',
+            'last_name' => '',
+            'username' => 'user@name',
+            'email' => 'invalid-email',
+            'password' => 'short',
+            'password_confirmation' => 'password123',
+        ];
+
+        $request = new CreateUserRequest();
+        $validator = validator($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+    }
+}
