e8076738bd07c6e725de6d1b3de11757115dd765
adrean-hipe <adrean@bpoc.co.jp>
2023-10-03 17:22:30 +0800

    Fixed @property

--- laravel-app/app/Models/PostLike.php
+++ laravel-app/app/Models/PostLike.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
- * @property int @user_id
- * @property int @post_id
+ * @property int $user_id
+ * @property int $post_id
  */
 class PostLike extends Model
 {
