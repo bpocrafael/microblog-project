967440dc752ebe399fe5c43f324e693fff7ea87a
adrean-hipe <adrean@bpoc.co.jp>
2023-10-03 17:08:33 +0800

    Added @property for larastan

--- laravel-app/app/Models/PostLike.php
+++ laravel-app/app/Models/PostLike.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int @user_id
+ * @property int @post_id
+ */
 class PostLike extends Model
 {
     use HasFactory;
