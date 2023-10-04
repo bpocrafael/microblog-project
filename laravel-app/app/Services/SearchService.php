2ef009eb168a06d29e352d617112424fe3d0c873
adrean-hipe <adrean@bpoc.co.jp>
2023-10-04 15:43:12 +0800

    Recommiting modified files

--- laravel-app/app/Services/SearchService.php
+++ laravel-app/app/Services/SearchService.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Collection;
 
 class SearchService
 {
-	/**
-	 * To search the User table using the Eloquent of matching results.
-	 * @return Collection<User>
-	 */
-	public function searchUser(String $query): Collection
-	{
-		return User::where('username', 'like', '%' . $query . '%')->get();
-	}
+    /**
+     * To search the User table using the Eloquent of matching results.
+     * @return Collection<User>
+     */
+    public function searchUser(String $query): Collection
+    {
+        return User::where('username', 'like', '%' . $query . '%')->get();
+    }
 }
