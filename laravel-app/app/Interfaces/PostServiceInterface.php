d4c196f3c5a9f68fe59b5772184fe7f1a5f736ce
adrean-hipe <adrean@bpoc.co.jp>
2023-10-10 11:31:57 +0800

    Added share post feature
    fix code by larastan check attempt 1
    
    fix code by larastan check attempt 2
    
    fix code by larastan check attempt 2
    
    fix code by larastan check attempt 4
    
    fix code by larastan check attempt 5
    
    fix code based on cs fixer attempt 1
    
    fix code based on review, removed comments
    
    Added comments to methods, removed docblocks
    
    Fix code by larastan check attemp 1

--- laravel-app/app/Interfaces/PostServiceInterface.php
+++ laravel-app/app/Interfaces/PostServiceInterface.php
@@ -8,8 +8,14 @@ use App\Models\UserPost;
 interface PostServiceInterface
 {
     /**
-     * @param User $user
      * @param array<mixed, mixed> $validatedData
      */
     public function createPost(User $user, array $validatedData): UserPost;
+
+    /**
+     * @param array<string, mixed> $validatedData
+     */
+    public function updatePost(UserPost $post, array $validatedData): bool;
+
+    public function sharePost(UserPost $post): UserPost;
 }
