f4d80ac850a4fb8faa492f92d4fa5749f2dd62fe
adrean-hipe <adrean@bpoc.co.jp>
2023-10-10 09:04:15 +0800

    removed unnecessary comments

--- laravel-app/app/Http/Controllers/FollowController.php
+++ laravel-app/app/Http/Controllers/FollowController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\RedirectResponse;
 
 class FollowController extends Controller
 {
-    /**
-     * To display all the followers-following of the user
-     */
     public function show(User $user): View
     {
         $followers = $user->followers()->get();
@@ -23,9 +20,6 @@ class FollowController extends Controller
         ]);
     }
 
-    /**
-     * To follow other user account.
-     */
     public function update(User $user): RedirectResponse
     {
         /** @var User $authUser */
@@ -38,9 +32,6 @@ class FollowController extends Controller
         return redirect()->back()->with($success);
     }
 
-    /**
-     * To unfollow a user account.
-     */
     public function destroy(User $user): RedirectResponse
     {
         /** @var User $authUser */
