d80e0d89e132f1357a526cbec2d8cd911e95af9b
mikco-hipe <mikco@bpoc.co.jp>
2023-10-11 15:39:43 +0800

    Run larastan attempt nad cs fixer: 5

--- laravel-app/app/Http/Requests/UpdateProfileImageRequest.php
+++ laravel-app/app/Http/Requests/UpdateProfileImageRequest.php
@@ -9,7 +9,7 @@ class UpdateProfileImageRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      */
-    public function rules()
+    public function rules(): array /* @phpstan-ignore-line */
     {
         return [
             'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
