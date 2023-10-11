9d6237bc8c90bc79b9b39b0b6930e8567cabda9d
adrean-hipe <adrean@bpoc.co.jp>
2023-10-11 15:29:30 +0800

    added ignore line for phpstan

--- laravel-app/app/Interfaces/PostServiceInterface.php
+++ laravel-app/app/Interfaces/PostServiceInterface.php
@@ -9,15 +9,13 @@ interface PostServiceInterface
 {
     /**
      * To create a new post of the user.
-     * @param array<mixed, mixed> $validatedData
      */
-    public function createPost(User $user, array $validatedData): UserPost;
+    public function createPost(User $user, array $validatedData): UserPost; // @phpstan-ignore-line
 
     /**
      * To update an existing post of the user.
-     * @param array<string, mixed> $validatedData
      */
-    public function updatePost(UserPost $post, array $validatedData): bool;
+    public function updatePost(UserPost $post, array $validatedData): bool; // @phpstan-ignore-line
 
     /**
      * To reference an exsiting post in a new post.
