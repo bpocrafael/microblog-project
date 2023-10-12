b5066c567da1ddb2481799aca0b62339685c4e85
mikco-hipe <mikco@bpoc.co.jp>
2023-10-12 12:01:50 +0800

    removed  /* @phpstan-ignore-line */

--- laravel-app/app/Http/Requests/UpdateProfileImageRequest.php
+++ laravel-app/app/Http/Requests/UpdateProfileImageRequest.php
@@ -8,8 +8,9 @@ class UpdateProfileImageRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
+     * @return array<string>
      */
-    public function rules(): array /* @phpstan-ignore-line */
+    public function rules(): array
     {
         return [
             'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
