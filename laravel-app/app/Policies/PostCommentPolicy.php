87a979366531f0bb73ab37c708beade9de032d97
mikco-hipe <mikco@bpoc.co.jp>
2023-10-16 00:45:12 +0800

    fix/ post comment bugfix

---
+++ laravel-app/app/Policies/PostCommentPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\PostComment;
+use App\Models\User;
+
+class PostCommentPolicy
+{
+    /**
+     * Determines whether the user can delete his/her own comment.
+     */
+    public function delete(User $user, PostComment $comments): bool
+    {
+        return $user->id === $comments->user_id;
+    }
+}
