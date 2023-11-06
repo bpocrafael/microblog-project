4e486aef2af77c9e8bd0ce68c96e053c7d2f7e22
mikco-hipe <mikco@bpoc.co.jp>
2023-11-06 09:30:55 +0800

    resolved conflicts

--- laravel-app/resources/views/components/profile.blade.php
+++ laravel-app/resources/views/components/profile.blade.php
@@ -1,17 +1,36 @@
-@if ($authUser->id)
-	<a class="text-dark" href="{{ route('profile.show', $authUser->id) }}">
-		@if (!$authUser->image_path)
+{{-- <<<<<<< HEAD --}}
+{{-- <a class="text-dark" href="{{ route('profile.show', $user->id) }}">
+    @if ($user->image_path === "assets/images/user-solid.svg")
+        <div class="profile-button" id="profileButtonContainer1">
+            <div class="bg">
+                <div class="letter">
+                    {{ $user->first_letter }}
+                </div>
+            </div>
+        </div>
+    @else
+        <button class="custom-profile-button" id="profileButtonContainer1">
+            <div class="image-bg">
+                <img src="{{ asset($user->image_path) }}" alt="Profile Image">
+            </div>
+        </button>
+    @endif
+</a>
+======= --}}
+@if ($user->id)
+	<a class="text-dark" href="{{ route('profile.show', $user->id) }}">
+		@if (!$user->image_path)
 			<div class="profile-button" id="profileButtonContainer1">
 				<div class="bg">
 					<div class="letter">
-						{{ ucfirst(substr($authUser->full_name, 0, 1)) }}
+						{{ ucfirst(substr($user->full_name, 0, 1)) }}
 					</div>
 				</div>
 			</div>
 		@else
 			<button class="custom-profile-button" id="profileButtonContainer1">
 				<div class="image-bg">
-					<img src="{{ asset($authUser->image_path) }}" alt="Profile Image">
+					<img src="{{ asset($user->image_path) }}" alt="Profile Image">
 				</div>
 			</button>
 		@endif
@@ -35,3 +54,4 @@
 		@endif
 	</a>
 @endif
+{{-- >>>>>>> ac17480 (feat/profile image delete) --}}
